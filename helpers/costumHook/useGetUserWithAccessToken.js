20f4fb38a85acccee5576f2e91f4be0e7c7b6cfe
okanay <okanay@hotmail.com>
2023-03-10 14:52:31 +0300

    firebase-2.66

--- helpers/costumHook/useGetUserWithAccessToken.js
+++ helpers/costumHook/useGetUserWithAccessToken.js
@@ -1,67 +1,26 @@
-import {useEffect, useState} from "react";
-import {signOut, useSession} from "next-auth/react";
+import { useState} from "react";
 import {GetFirebaseData} from "../Fetchs-Functions/GetFirebaseData";
 
-export const useGetUserWithAccessToken = (session, status) => {
+export const GetUserWithAccessToken = async (session, status) => {
 
-    const [response, setResponse] = useState({data: undefined, status: 'fetching', ok: false})
-    const [error, setError] = useState({error: false, message: ""})
-
-    console.log({ message : 'Custom Hook session : ', session : session})
-
-
-    useEffect(() => {
-        console.log('CustomHook : Effect Start')
-        if (status === "authenticated") {
-            console.log('CustomHook : if 1')
-            if (session?.user?.name?.accessToken)
+    const data = await GetFirebaseData("/api/getUserWithAccessToken", {
+            accessToken: session.user.name.accessToken,
+            collectionName : "users",
+            whereQuery: "accessToken"
+        }).then(data => {
+            if (data.data === undefined)
             {
-                console.log('costumHook 2')
-                GetFirebaseData("/api/getUserWithAccessToken", {
-                    accessToken: session.user.name.accessToken,
-                    collectionName : "users",
-                    whereQuery: "accessToken"
-                }).then(data => {
-                    console.log('costumHook 3')
-                    if (data.data === undefined)
-                    {
-                        console.log('costumHook 4')
-                        throw new Error('No Data Found')
-                    }
-
-                    setResponse({data: data.data, status: 'Success!', ok: true})
-
-                }).catch(err => {
-                    console.log('costumHook 5')
-                    setResponse({data: undefined, status: 'error', ok: false})
-                    setError(err)
-                    // signOut({callback : "/signin"})
-                    // SafeSignOutFirebaseAndNextAuth()
-                })
+                throw new Error('No Data Found')
             }
-            else{
-                console.log('CustomHook : if 1 : else 1')
-                return
-            }
-        }
-        else if (status === "unauthenticated")
-        {
-            console.log('CustomHook : else if 1')
-            setResponse({data: undefined, status: 'unauthenticated', ok: false})
-            setError({error: true, message: "User Unauthenticated!"})
-        }
-        else if (status === "loading")
-        {
-            console.log('CustomHook : else if 2')
-            setResponse({data: undefined, status: 'loading', ok: false})
-            setError({error: true, message: "Session Data Loading.."})
-        }
 
-    }, [session, status])
+            const response = {data: data.data, status: 'Success', ok: true}
+            return response
 
-    console.log({ message : 'Custom Hook status : ', status : status})
-    console.log({ message : 'Custom Hook session : ', session : session})
-    console.log({ message : 'Custom Hook session.user : ', session : session.user})
-    console.log('Return Custom Hook : ')
-    return [response, error]
+        }).catch(err => {
+
+            const response = {data: undefined, status: 'error', ok: false}
+            return response
+        })
+
+    return data
 }
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -3,32 +3,31 @@ import {getSession, useSession} from "next-auth/react";
 
 import {motion as m} from "framer-motion";
 import {animationStore} from "../../framer-motion-animations/store";
-import {useGetUserWithAccessToken} from "../../helpers/costumHook/useGetUserWithAccessToken";
-import {useEffect} from "react";
+import {GetUserWithAccessToken} from "../../helpers/costumHook/useGetUserWithAccessToken";
+import {useEffect, useState} from "react";
 
 const ProfileIndex = () => {
 
-
+    const [user, setUser] = useState({})
     const {data: session, status} = useSession()
-    const [user, error] = useGetUserWithAccessToken(session,status)
-
-    console.log({ message : 'Profile Page status : ', status : status})
-    console.log({ message : 'Profile Page session : ', session : session})
-    console.log({ message : 'Profile Page session.user : ', session : session.user})
-    console.log({ message : 'Profile Page error : ', error : error})
-    console.log({ message : 'Profile Page user : ', user : user})
-
 
     useEffect(() => {
-        console.log('Profile Page Effect : ')
+
+        if (session.user?.name.accessToken !== undefined)
+        {
+            GetUserWithAccessToken(session,status).then(response => {
+                setUser({...response})
+            })
+        }
+
     }, [status,session])
+
 
     return (
         <ProfileLayout>
             <m.div variants={animationStore.main} initial='initial' animate='animate' exit={'exit'}>
                 <h1 className={'text-3xl'}>Profile Index</h1>
                 <div className={'flex flex-col gap-1 mt-2 text-sm font-light bg-skin-theme-100/20 py-2 px-4 rounded max-w-screen-laptop'}>
-
                     <div className={'mb-4 text-skin-theme-font-100 font-semibold'}>
                         <p>Fetch : <span className={'text-skin-theme-600'}>{user.ok ? "complete" : "try to fetch.."}</span></p>
                         <p>Fetch status : <span className={'text-skin-theme-600'}>{user.status === "Success!" ? "Success" : user.status + ".."}</span></p>
