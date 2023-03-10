cd1dd7030fee39f93ecc6f74f620afd365d8d7d6
okanay <okanay@hotmail.com>
2023-03-10 13:22:45 +0300

    firebase-2.50

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -4,14 +4,20 @@ import {getSession, useSession} from "next-auth/react";
 import {motion as m} from "framer-motion";
 import {animationStore} from "../../framer-motion-animations/store";
 import {useGetUserWithAccessToken} from "../../helpers/costumHook/useGetUserWithAccessToken";
-import {useEffect, useState} from "react";
-import {SafeSignOutFirebaseAndNextAuth} from "../../helpers/Fetchs-Functions/SafeSignOutFirebaseAndNextAuth";
+import {useEffect} from "react";
 
 const ProfileIndex = () => {
 
     const [user, error] = useGetUserWithAccessToken("users")
     const {data: session, status} = useSession()
 
+    useEffect(() => {
+        console.log(1)
+
+        return () => {
+            console.log('2')
+        }
+    }, [])
     console.log(error)
     console.log(user)
 
