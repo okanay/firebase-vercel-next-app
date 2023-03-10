75a055be630cab1832e29029b42ce03f22da0f4c
okanay <okanay@hotmail.com>
2023-03-10 13:43:54 +0300

    firebase-2.63

--- helpers/costumHook/useGetUserWithAccessToken.js
+++ helpers/costumHook/useGetUserWithAccessToken.js
@@ -8,25 +8,28 @@ export const useGetUserWithAccessToken = (session, status) => {
     const [error, setError] = useState({error: false, message: ""})
 
     useEffect(() => {
-
+        console.log('costumHook Effect')
         if (status === "authenticated") {
+            console.log('costumHook 1')
             if (session?.user?.name?.accessToken)
             {
+                console.log('costumHook 2')
                 GetFirebaseData("/api/getUserWithAccessToken", {
                     accessToken: session.user.name.accessToken,
                     collectionName : "users",
                     whereQuery: "accessToken"
                 }).then(data => {
-
+                    console.log('costumHook 3')
                     if (data.data === undefined)
                     {
+                        console.log('costumHook 4')
                         throw new Error('No Data Found')
                     }
 
                     setResponse({data: data.data, status: 'Success!', ok: true})
 
                 }).catch(err => {
-
+                    console.log('costumHook 5')
                     setResponse({data: undefined, status: 'error', ok: false})
                     setError(err)
                     // signOut({callback : "/signin"})
@@ -36,16 +39,19 @@ export const useGetUserWithAccessToken = (session, status) => {
         }
         else if (status === "unauthenticated")
         {
+            console.log('costumHook 6')
             setResponse({data: undefined, status: 'unauthenticated', ok: false})
             setError({error: true, message: "User Unauthenticated!"})
         }
         else if (status === "loading")
         {
+            console.log('costumHook 7')
             setResponse({data: undefined, status: 'loading', ok: false})
             setError({error: true, message: "Session Data Loading.."})
         }
 
     }, [session, status])
 
+    console.log('costumHook 8')
     return [response, error]
 }
